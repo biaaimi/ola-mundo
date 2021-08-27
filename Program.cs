95c0c6ede064a398b63455eee12ba2cd787d92bf
biaaimi <biagioni.aimi@gmail.com>
2021-08-27 18:35:46 -0300

    ola mundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicios_etec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Olá usuário, eu me chamo Beatriz.");
+        }
+    }
+}
